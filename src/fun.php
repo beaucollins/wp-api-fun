47eaf16a486f2c433f711c3d316d50c8d743d810
Beau Collins <beau@collins.pub>
2020-03-05 14:27:35 -0800

    The plugin

---
+++ src/fun.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: Totes
+ */
+/**
+ * Responds to a REST request with text/plain "You did it!"
+ *
+ * @param WP_REST_Request $request
+ * @return WP_REST_Response
+ */
+function totally_not_buggy( $request ) {
+   return new WP_REST_Response( [ 'status' => 'not buggy' ] );
+}
+
+/**
+ * @param string $path
+ * @param (callable(WP_REST_Request):(WP_REST_Response|WP_Error|JSONSerializable)) $handler
+ * @return void
+ */
+function totes_register_api_endpoint( $path, $handler ) {
+   register_rest_route( 'totes', $path, [
+      'callback' => $handler
+   ] );
+}
+
+add_action( 'rest_api_init', function() {
+   totes_register_api_endpoint('not-buggy', 'totally_not_buggy');
+} );
